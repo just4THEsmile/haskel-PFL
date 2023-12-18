6933440ea59b8a674cf27e18fd88ac64278d6501
Rodrigo Póvoa <up202108890@up.pt>
2023-12-18 21:52:35 +0000

    ficha3 feita

--- ficha3.hs
+++ ficha3.hs
@@ -30,3 +30,31 @@ myfoldr1 function list = foldr (\x acc -> function acc x) (last list) (init list
 mdc:: Integral a => a -> a -> a
 mdc a b = fst(until (\(a, b) -> b == 0) (\(a, b) -> (b, mod a b)) (a, b))
 
+(+++) :: [a]->[a]->[a]
+(+++) a b = foldr (\x acc -> [x] ++ acc ) b a
+
+myconcat :: [[a]] -> [a]
+myconcat list = foldr (\x acc -> x ++ acc) [] list
+
+myreverse :: [a] -> [a]
+myreverse list = foldr(\x acc -> acc ++ [x]) [] list
+
+myreverse1 :: [a] -> [a]
+myreverse1 list = foldl(\acc x -> [x] ++ acc) [] list
+
+myelem :: Eq a => a -> [a] -> Bool
+myelem elem list = any(\x -> x == elem) list
+
+palavras :: String -> [String]
+palavras "" = []
+palavras s  = palavra : palavras resto
+  where
+    palavra = takeWhile (/= ' ') s
+    resto   = dropWhile (== ' ') $ dropWhile (/= ' ') s
+
+despalavras :: [String] -> String
+despalavras = foldr (\x acc -> x ++ " " ++ acc) ""
+
+myScanl :: (b -> a -> b) -> b -> [a] -> [b]
+myScanl _ z []     = [z]
+myScanl f z (x:xs) = z : myScanl f (f z x) xs
