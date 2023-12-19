d2467deb531aad865bf9e5e313ef90e3a51385a7
Rodrigo Póvoa <up202108890@up.pt>
2023-12-19 17:31:53 +0000

    proj

---
+++ proj/imperative.hs
@@ -0,0 +1,15 @@
+-- Part 2
+
+-- TODO: Define the types Aexp, Bexp, Stm and Program
+
+-- compA :: Aexp -> Code
+compA = undefined -- TODO
+
+-- compB :: Bexp -> Code
+compB = undefined -- TODO
+
+-- compile :: Program -> Code
+compile = undefined -- TODO
+
+-- parse :: String -> Program
+parse = undefined -- TODO
