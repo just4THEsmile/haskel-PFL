9dc63a5daa40d824ebd48bca45bda0836ad93ffe
Rodrigo Póvoa <up202108890@up.pt>
2023-12-22 15:45:17 +0000

    1 done

--- proj/low_level_machine.hs
+++ proj/low_level_machine.hs
@@ -13,16 +13,7 @@ data Expr = AddExpr Expr Expr
     | Num Int
     deriving (Eq, Show)
 
-
-
---Find Value of a Variable
-stateLookup:: State -> String -> DataType
-stateLookup [] v = error ("Variable " ++ v ++ " not found") 
-stateLookup ((var, value):xs) v
-  | var == v = value
-  | otherwise = stateLookup xs v
-
-
+-- Execute Instructions
 exec :: (Code, Stack DataType, State) -> (Code, Stack DataType, State)
 
 --Empty
@@ -77,7 +68,11 @@ exec (Push n:code, stack, state) = (code, push (Number n) stack, state)
 
 --Fetch x Instruction
 exec(Fetch x:code, stack, state) = (code, push (stateLookup state x) stack, state)
-
+  where
+    stateLookup [] v = error ("Variable " ++ v ++ " not found") 
+    stateLookup ((var, value):xs) v
+      | var == v = value
+      | otherwise = stateLookup xs v
 
 --Store x Instruction
 exec (Store x:code, stack, state) = (code, pop stack, storeVal state (x, top stack))
@@ -129,52 +124,84 @@ exec (Loop code1 code2:code, stack, state) =
 exec (Noop:code, stack, state) = (code, stack, state)
 
 
-
+-- Convert From DataType to String
 convertStr :: DataType -> String
 convertStr TT = "True"
 convertStr FF = "False"
 convertStr (Number n) = show n
 
+-- Convert From Stack to String
 stack2Str :: Stack DataType -> String
 stack2Str stk
   | isEmpty stk = ""
   | otherwise = convertStr (top stk) ++ remainingStack
   where
     remainingStack = if isEmpty (pop stk) then "" else "," ++ stack2Str (pop stk)
-    
-state2Str :: State -> String
-state2Str [] = ""
-state2Str [(var, value)] = var ++ "=" ++ convertStr value
-state2Str ((var, value):xs) = var ++ "=" ++ convertStr value ++ "," ++ state2Str xs
 
+-- Sort State, According to testAssembler cases
+sortState :: State -> State
+sortState [] = []
+sortState (x:xs) = insert x (sortState xs)
+  where
+    insert x [] = [x]
+    insert (var,value) ((var2,value2):xs)
+      | var < var2 = (var,value):(var2,value2):xs
+      | otherwise = (var2,value2):insert (var,value) xs
+
+-- Convert From State to String
+state2Str :: State -> String
+state2Str state = state2StrSorted (sortState state)
+  where
+    state2StrSorted [] = ""
+    state2StrSorted [(var, value)] = var ++ "=" ++ convertStr value
+    state2StrSorted ((var, value):xs) = var ++ "=" ++ convertStr value ++ "," ++ state2Str xs
+
+
+-- Run Machine
 run :: (Code, Stack DataType, State) -> (Code, Stack DataType, State)
 run ([],stack,state) = ([],stack,state) 
 run (code,stack,state) = run $ exec (code,stack,state)
 
-
+-- Test Assembler
 testAssembler :: Code -> (String, String)
 testAssembler code = (stack2Str stack, state2Str state)
   where (_,stack,state) = run(code, createEmptyStack, createEmptyState)
 
 main :: IO()
 main = do
-    let stack = push (Number 1) $ push TT $ push FF createEmptyStack
-    print stack
-    let result = stack2Str stack
-    print result
-    
-    -- testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
+    --testAssembler [Push 10,Push 4,Push 3,Sub,Mult] == ("-10","")
+    --print $ testAssembler [Push 10,Push 4,Push 3,Sub,Mult]
+
     -- testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"] == ("","a=3,someVar=False,var=True")
+    --print $ testAssembler [Fals,Push 3,Tru,Store "var",Store "a", Store "someVar"]
+
     -- testAssembler [Fals,Store "var",Fetch "var"] == ("False","var=False")
+    --print $ testAssembler [Fals,Store "var",Fetch "var"]
+
     -- testAssembler [Push (-20),Tru,Fals] == ("False,True,-20","")
+    -- print $ testAssembler [Push (-20),Tru,Fals]
+
     -- testAssembler [Push (-20),Tru,Tru,Neg] == ("False,True,-20","")
+    -- print $ testAssembler [Push (-20),Tru,Tru,Neg]
+
     -- testAssembler [Push (-20),Tru,Tru,Neg,Equ] == ("False,-20","")
+    -- print $ testAssembler [Push (-20),Tru,Tru,Neg,Equ]
+
     -- testAssembler [Push (-20),Push (-21), Le] == ("True","")
+    -- print $ testAssembler [Push (-20),Push (-21), Le]
+
     -- testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
+    -- print $ testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"]
+
     -- testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
+    -- print $ testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]]
+
     -- If you test:
     -- testAssembler [Push 1,Push 2,And]
+    -- print $ testAssembler [Push 1,Push 2,And]
     -- You should get an exception with the string: "Run-time error"
+
     -- If you test:
     -- testAssembler [Tru,Tru,Store "y", Fetch "x",Tru]
+    -- print $ testAssembler [Tru,Tru,Store "y", Fetch "x",Tru]
     -- You should get an exception with the string: "Run-time error"
